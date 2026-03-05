7249c729dc4ba3bd5a4304307703f96ecc5e1f8d
kori756 <khorirachmatr@gmail.com>
2026-03-05 18:34:39 +0700

    menambahkan bubblesort

--- bubblesort_b.cpp
+++ bubblesort_b.cpp
@@ -24,9 +24,36 @@ void input()
     cout << "Masukan Elemen Array" << endl;
     cout << "===================" << endl;
 
-    for (int i = 0; i < n; i++)                     // looping dengan i dimulai dari 0 hingga n
+    for (int i = 0; i < n; i++)                     // looping dengan i dimulai dari 0 hingga n-1
     {
         cout << "Data ke-"  << (1 + 1) << ": ";     // output ke layar
         cin >> arr[i];                              // input dri pengguna
     }
+}
+void bubbleSortArray()
+{                                               //prosedur untuk mengurutkan array dengan metode bubble sort
+    int pass = 1;                               //step1
+
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++) 
+        { //step 2
+            if (arr[j] > arr[j + 1])
+            {   // step3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] + temp;
+            }
+      }
+      pass += 1; //step 4 
+
+      cout << "\nPass " << pass - 1 << ": "; //number of pass
+      for (int k = 0; k < n; k++)
+      {
+        cout << arr[k] << " "; // menampilkan data pada number of pass
+      }
+      cout << endl;
+    
+    } while (pass <= n - 1); //step 5
 }
